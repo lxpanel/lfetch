692daba51f56ab4ea71fdc30e718da94fd74b402
lxpanel <96754598+lxpanel@users.noreply.github.com>
2022-04-12 17:45:20 -0400

    lfetch code
    Lfetch code with different ASCII logos.

---
+++ lfetch-bsd.lua
@@ -0,0 +1,21 @@
+print("88                             88")
+print("88                             88")
+print("88                             88")
+print("88,dPPYba,  ,adPPYba,  ,adPPYb,88")
+print("88P'     8a I8[    '  a8      Y88")
+print("88       d8  ==Y8ba,  8b       88")
+print("88b,   ,a8+ aa    +8I +8a,   ,d88")
+print("8Y+Ybbd8++  ++YbbdP++  ++8bbdP+Y8")	
+print("OS:") dofile("osname.lua")
+print("HOST:") print(io.popen('uname -n'):read('*l'))
+print("OS CLOCK TIME:")
+print(os.clock())
+print("MEMORY USAGE (kb):")
+print(collectgarbage("count"))
+print("DATE:")
+print(os.date("%d.%m.%Y"))
+--[[
+Credits
+https://github.com/bluebird75/lua_get_os_name/blob/master/get_os_name.lua
+https://ascii.co.uk/art/bsd
+--]]
---
+++ lfetch.lua
@@ -0,0 +1,12 @@
+print("OS:") dofile("osname.lua")
+print("HOST:") print(io.popen('uname -n'):read('*l'))
+print("OS CLOCK TIME:")
+print(os.clock())
+print("MEMORY USAGE (kb):")
+print(collectgarbage("count"))
+print("DATE:")
+print(os.date("%d.%m.%Y"))
+--[[
+Credits
+https://github.com/bluebird75/lua_get_os_name/blob/master/get_os_name.lua
+--]]
